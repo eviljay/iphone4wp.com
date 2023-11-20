8ef28955b11ae438ba5c67d10de67fbf6a120f6c
evgerush <evgencherkashyn@gmail.com>
2023-11-21 00:18:11 +0200

    asd

---
+++ wp-content/plugins/qr-code-tag/js/qrct.js_ver=3.5.1.js
@@ -0,0 +1,33 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+/*
+ * QR Code Tag Wordpress Plugin Javascript jQuery Handler v1.0
+ * http://spreendigital.de/blog/wordpress
+ *
+ * Copyright (c) 2009 Dennis D. Spreen
+ *
+ * Dual licensed under the MIT and GPL licenses:
+ *   http://www.opensource.org/licenses/mit-license.php
+ *   http://www.gnu.org/licenses/gpl.html
+ */;
+jQuery(document).ready(function($) {
+	$("span.qrcttooltip").tooltip({
+	   	bodyHandler: function() {
+			return $("<img/>").attr("src", this.tooltipText); 
+	   	},
+		track: true, showURL: false, delay: 1, id: "qrcttooltip"
+	})
+  });
+
+}
+
